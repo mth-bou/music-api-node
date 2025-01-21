913a53d9c925896d7c7fbedd531713b5d07c2031
Mathieu <mathieu.boucher@epsi.fr>
2025-01-21 11:50:55 +0100

    implements musics controller and musics router

---
+++ src/controllers/musicController.js
@@ -0,0 +1,46 @@
+const musicModel = require('../models/musicModel');
+
+const getAllMusics = (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify(musicModel.getMusics()));
+}
+
+const getMusic = (req, res, id) => {
+  const music = musicModel.getMusicById(req.params.id);
+  if (music) {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(music));
+  } else {
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Music not found' }));
+  }
+}
+
+const createMusic = (req, res, body) => {
+  const newMusic = musicModel.addMusic(JSON.parse(body));
+  res.writeHead(201, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify(newMusic));
+}
+
+const updateMusic = (req, res, id, body) => {
+  const updatedMusic = musicModel.addMusic(id, JSON.parse(body));
+  if (updatedMusic) {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(updatedMusic));
+  } else {
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Music not found' }));
+  }
+}
+
+const deleteMusic = (req, res, id) => {
+  if (musicModel.deleteMusic(id)) {
+    res.writeHead(204, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Music deleted' }));
+  } else {
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Music not found' }));
+  }
+}
+
+module.exports = { getAllMusics, getMusic, createMusic, updateMusic, deleteMusic };
--- src/routes/musicRoutes.js
+++ src/routes/musicRoutes.js
@@ -1,1 +1,28 @@
+const url = require('url');
+const musicController = require('../controllers/musicController');
 
+const musicRouter = (req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+  const id = parsedUrl.pathname.split('/')[2];
+
+  if (req.method === 'GET' && parsedUrl.pathname === '/musics') {
+    musicController.getAllMusics(req, res);
+  } else if (req.method === 'GET' && id) {
+    musicController.getMusic(req, res, id);
+  } else if (req.method === 'POST' && parsedUrl.pathname === '/musics') {
+    let body = '';
+    req.on('data', (chunk) => (body += chunk.toString()));
+    req.on('end', () => musicController.createMusic(req, res, body));
+  } else if (req.method === 'PUT' && id) {
+    let body = '';
+    req.on('data', (chunk) => (body += chunk.toString()));
+    req.on('end', () => musicController.updateMusic(req, res, id, body));
+  } else if (req.method === 'DELETE' && id) {
+    musicController.deleteMusic(req, res, id);
+  } else {
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Route not found' }));
+  }
+}
+
+module.exports = musicRouter;
